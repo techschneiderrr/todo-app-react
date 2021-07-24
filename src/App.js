58e317446e6a2154512c0f0437bd9e6837168b16
Manav Deep Singh Lamba <56251839+techschneiderrr@users.noreply.github.com>
2021-07-24 14:16:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,40 +3,51 @@ import Header from './my components/Header';
 import { Todos } from "./my components/Todos";
 import { Footer } from "./my components/Footer";
 import { Addtodo } from "./my components/Addtodo";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
 
-  const [todos, settodos] = useState([
-  {
-    sno : 1,
-    title : "Check out this app",
-    desc : "This is my first try at a react application so I hope you will like it"
-  },
-  {
-    sno : 2,
-    title : "Check me out",
-    desc : "You can hae a look at the projects made by me by clicking the 'meet the creator' link in the nav"
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
   }
-]);
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
 
 const deleteFunc= (todo)=>{
   // console.log("i am a delete function",todo)
-  settodos(todos.filter((e)=>{
+  setTodos(todos.filter((e)=>{
      return e!==todo;
   }))
+  console.log("deleted", todos)
+  localStorage.setItem("todos", JSON.stringify(todos));
+
 }
 
 const addTodo = (title,desc)=>{
-let sno = todos[todos.length - 1].sno + 1;
-const mytodo = {
-  sno : sno,
-  title: title,
-  desc: desc
+  console.log("I am adding this todo", title, desc)
+  let sno;
+  if (todos.length === 0) {
+    sno = 0;
+  }
+  else {
+    sno = todos[todos.length - 1].sno + 1;
+  }
+  const myTodo = {
+    sno: sno,
+    title: title,
+    desc: desc,
+  }
+  setTodos([...todos, myTodo]);
+  console.log(myTodo);
 }
-console.log(mytodo);
-settodos([...todos,mytodo]);
-}
+
+const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
 
   return (
     <> 
